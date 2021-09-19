b5d2c1859a84a46255fb48e0b653490b9aac6283
Igor Eduardo Mascarenhas <50914198+IgorEM@users.noreply.github.com>
2021-09-19 15:21:29 -0300

    class

---
+++ contador_palavras.py
@@ -0,0 +1,14 @@
+def contador_letras(lista_palavras):
+    lista1 = []
+    for x in lista_palavras:
+        contador = len(x)
+        lista1.append(contador)
+    return lista1
+
+def teste():
+    return 'teste'
+
+if __name__ == '__main__':
+    lista = ['cachorro', 'gato','elefante']
+    total_letras = contador_letras(lista)
+    print('Total de letras por palavras da lista: {}'.format(total_letras))
